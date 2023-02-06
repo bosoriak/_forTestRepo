ad5db3bf4c02fca27a2dc1259d875816539a7a42
bosoriak <lucia@klaucova.ch>
2023-02-06 19:24:32 +0100

    We love enter anyhow!

--- _appItself/ApplicationUnderTest/UiTests/uiTests/uiTests.cs
+++ _appItself/ApplicationUnderTest/UiTests/uiTests/uiTests.cs
@@ -24,6 +24,7 @@ namespace uiTests
             WindowsElement button = session.Driver.FindElementByAccessibilityId("ButtonId");
             Thread.Sleep(1000);
             button.Click();
+
             WindowsElement textBox = session.Driver.FindElementByAccessibilityId("TextBoxId");
             Assert.That(textBox.GetAttribute("Value.Value"), Is.Not.Null);
         }
