ea31acf9e388da52b48224146b58eacd92ae8f9a
bosoriak <lucia@klaucova.ch>
2023-02-06 19:26:38 +0100

    Enters! Enters everywhere!

--- _appItself/ApplicationUnderTest/UiTests/uiTests/uiTests.cs
+++ _appItself/ApplicationUnderTest/UiTests/uiTests/uiTests.cs
@@ -12,7 +12,9 @@ namespace uiTests
         public void Setup()
         {
             session = new();
+
             session.StartSession();
+
             Thread.Sleep(5000);
         }
 
@@ -22,17 +24,22 @@ namespace uiTests
         public void Click()
         {
             WindowsElement button = session.Driver.FindElementByAccessibilityId("ButtonId");
+
             Thread.Sleep(1000);
+
             button.Click();
 
             WindowsElement textBox = session.Driver.FindElementByAccessibilityId("TextBoxId");
+
             Assert.That(textBox.GetAttribute("Value.Value"), Is.Not.Null);
         }
 
         [OneTimeTearDown]
         public void TearDown()
         {
+
             session.TearDown();
+
         }
     }
 }
