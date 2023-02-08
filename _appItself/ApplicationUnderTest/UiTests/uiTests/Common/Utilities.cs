121aebecf94ade265f00ae644e111162ee7ccdbf
bosoriak <lucia@klaucova.ch>
2023-02-08 17:25:48 +0100

    add utilities for tryFunctionWithTimeout2

---
+++ _appItself/ApplicationUnderTest/UiTests/uiTests/Common/Utilities.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uiTests.Common
+{
+    public class Utilities
+    {
+        public static void TryExecuteWithTimeout(Action function, Func<bool> condition, int timeout, int step)
+        {
+            int elapsedTime = 0;
+            while (elapsedTime < timeout && !condition())
+            {
+                try
+                {
+                    function();
+                    if (condition())
+                    {
+                        break;
+                    }
+                }
+                catch (Exception)
+                {
+                    elapsedTime += step;
+                    Thread.Sleep(step);
+                }
+            }
+        }
+    }
+}
