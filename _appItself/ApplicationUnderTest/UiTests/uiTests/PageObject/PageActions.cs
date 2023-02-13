fa99bfb89108e05a5a6f8a8fa0f72ae62638b520
bosoriak <lucia@klaucova.ch>
2023-02-13 17:07:54 +0100

    add screenshot

---
+++ _appItself/ApplicationUnderTest/UiTests/uiTests/PageObject/PageActions.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium.Appium.Windows;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uiTests.PageObject
+{
+    public class PageActions
+    {
+        public static int CountDistinctResult(WindowsElement button, WindowsElement textBoxToGetValueFrom, int repetitionCount)
+        {
+            List<string> textsFromTextbox = new List<string>();
+
+            for (int i = repetitionCount; i > 0; i--)
+            {
+                button.Click();
+                string text = textBoxToGetValueFrom.GetAttribute("Value.Value");
+                textsFromTextbox.Add(text);
+            }
+
+            return textsFromTextbox.Distinct().Count();
+            
+       }
+    }
+}
